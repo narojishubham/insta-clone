8c2f03f7342823ce6ca4534b58eb5b724540207e
narojishubham1822 <shubham.naroji@gmail.com>
2021-12-15 15:08:16 +0530

    adding firebase to insta clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import  firebase  from "firebase/app";
+import React from "react";
+import "./App.css";
 
 function App() {
+  // const firebaseApp = firebase.app[0];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>React & Firebase</h1>
+      <h2>By @farazamiruddin</h2>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code> */}
     </div>
   );
 }
---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore";
+import "firebase/functions";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9nEPlPm_gaUhF6rBUrR2sVSfPTl0tW_E",
+  authDomain: "instragram-app-1bd98.firebaseapp.com",
+  projectId: "instragram-app-1bd98",
+  storageBucket: "instragram-app-1bd98.appspot.com",
+  messagingSenderId: "307019760257",
+  appId: "1:307019760257:web:bdd5858f40023e6c150380",
+};
+const app = initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
+export const auth = app.auth();
+export const db = app.database();
+export const firestoreDB = app.firestore();
