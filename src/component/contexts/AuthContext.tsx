6a812aa19ee537836ef769ca178c253f067882a0
narojishubham1822 <shubham.naroji@gmail.com>
2021-12-16 12:16:05 +0530

    adding auth

--- src/component/contexts/AuthContext.tsx
+++ src/component/contexts/AuthContext.tsx
@@ -1,16 +1,36 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { auth } from "../../firebase/firebaseConfig";
 
 const AuthContext = React.createContext();
+
+type IUserData = {
+  children: any;
+  username: string;
+  password: string;
+};
 
 export const useAuth = () => {
   return useContext(AuthContext);
 };
 
-export function AuthProvider({ children }) {
+export const AuthProvider: React.FC<IUserData> = ({ children }) => {
   const [currenntUser, setCurrentUser] = useState();
+
+  const Signup: React.FC<IUserData> = ({ username, password }) => {
+    return auth.createUserWithEmailAndPassword(username, password);
+  };
+  useEffect(() => {
+    // firebase func
+    const unsubscribe = auth.onAuthStateChanged((user: any) => {
+      setCurrentUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
   const value = {
     currenntUser,
+    Signup,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-}
+};
